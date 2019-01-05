aa5260a1030faf5aa280e591bc54be3a40a833f7
Vladimir Kalmykov <vladimirmorulus@gmail.com>
2019-01-05 08:56:12 +0300

    hotfix add constat CHANNEL

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ exports.PAYLOAD_PACK = Symbol("PAYLOAD_PACK");
 exports.RETURN_PAYLOAD = Symbol("RETURN_PAYLOAD");
 exports.CANCEL = Symbol("CANCEL");
 exports.CANCELLED = Symbol("CANCELLED");
+exports.CHANNEL = Symbol("CHANNEL");
